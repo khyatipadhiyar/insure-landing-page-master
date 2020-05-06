7c275832d4e862595a0b9db42fcf7af445a4341c
unknown <khyati11892@gmail.com>
2020-05-06 19:22:05 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+
+const hamburger=document.querySelector('#hamburger');
+const menu=document.querySelector('#menu');
+
+
+hamburger.addEventListener('click', () =>
+{		
+		hamburger.classList.toggle('show');
+		
+		menu.classList.toggle('show');
+		
+});
